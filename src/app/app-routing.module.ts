315ae3f854daa3382c3fc75cc75b5f48194e116c
Johnson Okorie <jokorie@inlaks.com>
2021-09-10 15:22:29 +0100

    added new features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ProductsComponent } from './products/products.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingPageComponent },
+  { path: 'about', component: AboutUsComponent},
+  { path: 'blog', component: BlogComponent},
+  { path: 'products', component: ProductsComponent},
+  { path: 'contact', component: ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LayoutModule } from './layout/layout.module';
+import { BlogComponent } from './blog/blog.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    LandingPageComponent,
+    BlogComponent,
+    ProductsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
+    LayoutModule,
     AppRoutingModule
   ],
   providers: [],
