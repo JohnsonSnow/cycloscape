7119a2c933046946362cfb8bde421137d6605c79
Johnson Okorie <jokorie@inlaks.com>
2021-10-03 12:15:32 +0100

    ok

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutUsComponent implements OnInit {
 
+  readMore: false;
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
+
+  displayMore = () => {
+    //this.readMore = true;
+  };
 
 }
